1a099e7bac82983847e1c1deb853344f2aa2d8c4
Poet <946261953@qq.com>
2026-07-14 22:37:20 +0800

    feat: 添加案例

--- 26-class.py
+++ 26-class.py
@@ -30,3 +30,39 @@ car2 = Car('Toyota', 'Corolla', 2016)
 
 # print(Car.wheels)
 # car1.drive()
+
+
+
+# Inheritance = Allows a class to inherit attributes and methods from another class 
+#               Helps with code reusability and extensibility 
+#               class Child(Parent)
+
+class Animal:
+    def __init__(self, name):
+        self.name = name
+        self.is_alive = True
+    def eat(self):
+        print(f'{self.name} is eating')
+    def sleep(self):
+        print(f'{self.name} is sleeping')
+
+class Dog(Animal):
+    def bark(self):
+        print(f'{self.name} is barking')
+
+class Cat(Animal):
+    def meow(self):
+        print(f'{self.name} is meowing')
+
+dog = Dog('Buddy')
+cat = Cat('Fluffy')
+
+# dog.bark()
+# cat.meow()
+
+
+# mulitple inheritance = inheritance from more than one parent class
+#                         class Child(Parent1, Parent2) 
+
+# muiltilevel inheritance = inheritance from a parent class which inherits from another parent class
+#                            C(B) <- B(A) <- A
